4796ea7a05d369f08d8e5bc75f7e5053d46aa79d
veryresto <veryresto@gmail.com>
2026-05-14 20:24:35 +0700

    refactor: move server file to src/ directory and remove unused project files

--- src/server.js
+++ src/server.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
 });
 
 // Static files
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
 // Health check
 app.get('/health', (req, res) => {
